58feb3b841d98fa46d159cc1487d1543a3ec46c2
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-02-04 21:57:42 +0800

    Add common resistors

--- ElecCalculator/ResistorIdentificationView.swift
+++ ElecCalculator/ResistorIdentificationView.swift
@@ -10,11 +10,15 @@ import SwiftUI
 struct ResistorIdentificationView: View {
     @State var resistor: Resistor = .init(resistance: 220, tolerance: 5)
 
+    // incremented whenever the resistor view needs updating
+    @State var updator: Int = 0
+
     var body: some View {
         List {
             Section {
                 EditableResistorView(resistor: $resistor)
                     .frame(height: 110)
+                    .id(updator)
             }
 
             Section {
@@ -37,6 +41,24 @@ struct ResistorIdentificationView: View {
                     Text("Minimum Value:")
                     Spacer()
                     Text("\(String(format: "%.2f", CGFloat(resistor.resistance) * CGFloat(100 - resistor.tolerance) / CGFloat(100))) Ω")
+                }
+            }
+
+            Section("Common Resistors") {
+                ForEach([220, 1000], id: \.self) { resistance in
+                    Button {
+                        resistor.resistance = resistance
+                        updator += 1
+                    } label: {
+                        HStack {
+                            ResistorView(resistor: .init(resistance: resistance, tolerance: 5))
+                                .scaleEffect(.init(0.2))
+                                .frame(width: 60, height: 30)
+                            Spacer()
+                            Text("\(resistance) Ω")
+                                .foregroundColor(.primary)
+                        }
+                    }
                 }
             }
         }
