1e73f2ae0a2c770a6b04032905f543068ca6dd6d
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-02-05 22:00:11 +0800

    Add animation to triangle view

--- ElecCalculator/UI/Components/EquationTriangleView.swift
+++ ElecCalculator/UI/Components/EquationTriangleView.swift
@@ -12,12 +12,35 @@ struct EquationTriangleView: View {
     @Updating var equation: Equation
     @Binding var selected: UnitTarget
 
+    @Namespace var namespace
+
+    init(equation: Equation,
+         selected: Binding<UnitTarget>) {
+        self._equation = <-equation
+        self._selected = selected
+    }
+
     var body: some View {
         VStack {
             viewForUnit(unit: equation.value, target: .value)
+                .background {
+                    if selected == .value {
+                        selectionColor
+                    }
+                }
             HStack {
                 viewForUnit(unit: equation.var1, target: .var1)
+                    .background {
+                        if selected == .var1 {
+                            selectionColor
+                        }
+                    }
                 viewForUnit(unit: equation.var2, target: .var2)
+                    .background {
+                        if selected == .var2 {
+                            selectionColor
+                        }
+                    }
             }
             .padding(.top, 10)
         }
@@ -25,6 +48,12 @@ struct EquationTriangleView: View {
             Color.primary
                 .frame(height: 1)
         }
+    }
+
+    var selectionColor: some View {
+        Color.green
+            .cornerRadius(10)
+            .matchedGeometryEffect(id: "selectionThing", in: namespace)
     }
 
     func viewForUnit(unit: EquationUnit, target: UnitTarget) -> some View {
@@ -46,9 +75,7 @@ struct EquationTriangleView: View {
             .padding(5)
             .background {
                 ZStack {
-                    if target == selected {
-                        Color.green
-                    } else {
+                    if target != selected {
                         Color.gray
                     }
                 }
